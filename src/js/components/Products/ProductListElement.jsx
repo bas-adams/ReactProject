a1d0db5bc467cc7be95aa69356fb706c45d1ff78
Basia Mapp <barbara.adamska@teradata.com>
2017-01-03 17:30:28 +0100

    v5

--- src/js/components/Products/ProductListElement.jsx
+++ src/js/components/Products/ProductListElement.jsx
@@ -5,18 +5,18 @@ import Heart from '../General/Heart.jsx';
 export default class ProductListElement extends React.Component{
     render(){
         return(
-
-                <div className="ProductListElement">
-                    <li className="productListItem">
-                        <img src="img/design_11.png" alt=""/>
-                        <div className="productListIcons">
-                            <StarRatingComponent maxNumberOfStars={this.props.maxNumberOfStars} checkedStars={this.props.checkedStars} />
-                            <Heart heartSelected={this.props.heartSelected} />
-                        </div>
-                        <h4>{this.props.productName}</h4>
-                        <p className="itemText">{this.props.productDesc}</p>
-                    </li>
+            <li className="ProductListElement">
+                <div className="productListItem">
+                    <img src="img/design_11.png" alt=""/>
+                    <div className="productListIcons">
+                        <StarRatingComponent maxNumberOfStars={this.props.maxNumberOfStars} checkedStars={this.props.checkedStars} />
+                        <Heart heartSelected={this.props.heartSelected} />
+                    </div>
+                    <h4>{this.props.productName}</h4>
+                    <p className="itemText">{this.props.productDesc}</p>
                 </div>
+            </li>
+
         );
     }
 }
